c335d2851604f46df9ca6d23561f20fa4d7c9e35
Kingsley Victor <javaprodigy56@gmail.com>
2020-07-09 13:04:06 -0400

    New commit

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-payment",
@@ -6,6 +7,16 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./payment.component.css"]
 })
 export class PaymentComponent implements OnInit {
+  
+  paymentFormGroup: FormGroup;
+
+  name: FormControl = new FormControl("", Validators.required);
+  email: FormControl = new FormControl("", [
+    Validators.required, Validators.email
+  ]);
+  constructor(fb: FormBuilder) {
+    this.paymentFormGroup = fb.group({});
+  }
   ngOnInit() {
     console.log("[Payment]");
   }
