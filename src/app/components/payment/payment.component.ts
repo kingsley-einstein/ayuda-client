4372f912983543ddc7a1a3551b27da4feb2fedd6
Kingsley Victor <javaprodigy56@gmail.com>
2020-07-09 13:34:18 -0400

    New commit

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms";
+import { PaymentService } from "../../services";
 
 @Component({
   selector: "app-payment",
@@ -14,10 +15,47 @@ export class PaymentComponent implements OnInit {
   email: FormControl = new FormControl("", [
     Validators.required, Validators.email
   ]);
-  constructor(fb: FormBuilder) {
-    this.paymentFormGroup = fb.group({});
+
+  @Output()
+  result: EventEmitter<string> = new EventEmitter<string>(true);
+
+  @Output()
+  message: EventEmitter<string> = new EventEmitter<string>(true);
+
+  constructor(fb: FormBuilder, private _service: PaymentService) {
+    this.paymentFormGroup = fb.group({
+      name: this.name,
+      email: this.email
+    });
   }
+
   ngOnInit() {
     console.log("[Payment]");
+  }
+
+  submit($event: any) {
+    $event.preventDefault();
+    this._service.createPayment().subscribe((res1) => {
+      console.log("__", res1.response);
+      this._service.initializePayment(this.paymentFormGroup.value).subscribe((res2) => {
+        window.open(res2.response.payment.authentication_url, "__blank");
+      },
+      (err) => {
+        this.result.emit("error");
+        this.message.emit(err.error.response || err.message);
+      },
+      () => {
+        this.result.emit("complete");
+        this.message.emit("Successfully initialized payment");
+      });
+    },
+    (err) => {
+      this.result.emit("error");
+      this.message.emit(err.error.response || err.message);
+    },
+    () => {
+      this.result.emit("error");
+      this.message.emit("Successfully created payment object");
+    });
   }
 }
