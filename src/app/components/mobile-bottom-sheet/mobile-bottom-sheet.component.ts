6bca19a8ec21ebf2929b122762fc28d2dcaa83cb
Kingsley Victor <javaprodigy56@gmail.com>
2020-07-11 04:01:31 -0400

    new commit

--- src/app/components/mobile-bottom-sheet/mobile-bottom-sheet.component.ts
+++ src/app/components/mobile-bottom-sheet/mobile-bottom-sheet.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
+import { Store, select } from "@ngrx/store";
 import { PayoutFormComponent } from "../payout-form/payout-form.component";
+import { AppState } from "../../states/app.state";
+import { selectReferral } from "../../selectors/referral.selector";
+import { GetReferral } from "../../actions/referral.action";
 
 @Component({
   selector: "app-mobile-bottom-sheet",
@@ -9,7 +13,9 @@ import { PayoutFormComponent } from "../payout-form/payout-form.component";
 })
 export class MobileBottomSheetComponent implements OnInit {
 
-  constructor(private _dialog: MatDialog) {}
+  constructor(private _dialog: MatDialog, private _store: Store<AppState>) {}
+
+  referralState = this._store.pipe(select(selectReferral));
 
   ngOnInit() {
     console.log("[Mobile Bottom Sheet]");
@@ -19,5 +25,22 @@ export class MobileBottomSheetComponent implements OnInit {
     this._dialog.open(PayoutFormComponent, {
       closeOnNavigation: true
     });
+  }
+
+  copyReferralLink($code: any) {
+    const link$ = window.location.host + "/registration/" + $code;
+    navigator.clipboard.writeText(link$).then(() => {
+      console.log("Copied referral link.");
+    });
+  }
+
+  copyReferralCode($code: any) {
+    navigator.clipboard.writeText($code).then(() => {
+      console.log("Copied referral code.");
+    });
+  }
+
+  getReferral() {
+    this._store.dispatch(new GetReferral());
   }
 }
