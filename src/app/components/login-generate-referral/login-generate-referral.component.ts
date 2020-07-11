3951480b749d1e418ad8d9f9e13a1adb2492975c
Kingsley Victor <javaprodigy56@gmail.com>
2020-07-11 10:15:41 -0400

    New commit

--- src/app/components/login-generate-referral/login-generate-referral.component.ts
+++ src/app/components/login-generate-referral/login-generate-referral.component.ts
@@ -7,6 +7,18 @@ import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms"
   styleUrls: ["./login-generate-referral.component.css"]
 })
 export class LoginGenerateReferralComponent implements OnInit {
+
+  amountType: { name: string, value: number }[] = [
+    { name: "#500 - #4000", value: 500 },
+    { name: "#1000 - #8000", value: 1000 },
+    { name: "#2000 - #16000", value: 2000 },
+    { name: "#5000 - #40000", value: 5000 },
+    { name: "#10000 - #80000", value: 10000 },
+    { name: "#20000 - #160000", value: 20000 },
+    { name: "#100000 - #800000", value: 100000 }
+  ];
+
+  selectedAmountType = 500;
   
   constructor() {}
   
