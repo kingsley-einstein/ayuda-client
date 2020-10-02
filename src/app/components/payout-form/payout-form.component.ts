301fa7f3948632aada9d7c67be8b7e6edc24d895
Kingsley Victor <javaprodigy56@gmail.com>
2020-10-02 19:49:03 +0100

    new commit

--- src/app/components/payout-form/payout-form.component.ts
+++ src/app/components/payout-form/payout-form.component.ts
@@ -28,6 +28,18 @@ export class PayoutFormComponent implements OnInit {
   ];
   selectedBank = "first_bank_of_nigeria";
 
+  amountType: { name: string, value: number }[] = [
+    { name: "5h for 4k", value: 500 },
+    { name: "1k for 8k", value: 1000 },
+    { name: "2k for 16k", value: 2000 },
+    { name: "5k for 40k", value: 5000 },
+    { name: "10k for 80k", value: 10000 },
+    { name: "20k for 160k", value: 20000 },
+    { name: "100k for 800k", value: 100000 }
+  ];
+
+  selectedAmountType = 500;
+
   fg: FormGroup;
 
   name: FormControl = new FormControl("", Validators.required);
@@ -55,7 +67,7 @@ export class PayoutFormComponent implements OnInit {
       console.log("____", res1);
       this._service.initializetransfer({
         recipientCode: res1.response.recipient.data.recipient_code,
-        amountType: parseInt(localStorage.getItem("amountType"), 10)
+        amountType: this.selectedAmountType
       }).subscribe((res2) => {
         console.log("_____", res2.response);
       },
